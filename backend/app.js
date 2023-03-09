50ebd66f58611d901362af580a27aa175f279d2b
KatsuroAlex <katsuroay@mail.ru>
2023-03-09 15:54:59 +0400

    test with cors

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-// const cors = require('cors');
+const cors = require('cors');
 
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -12,13 +12,13 @@ const router = require('./routes');
 const handleErrors = require('./middlewares/handleErrors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const path = require('path');
-// const corsOption = require('./middlewares/cors');
+const corsOption = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 // app.use('/api', require('../router'));
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 // app.use(express.static(path.join(__dirname, '../frontend/build')));  
 app.use(bodyParser.json());
 
@@ -28,7 +28,7 @@ app.use(cookieParser());
 
 
 app.use(requestLogger); // логгер запросов
-// app.use(cors(corsOption));
+app.use(cors(corsOption));
 
 
 app.get('/crash-test', () => {
