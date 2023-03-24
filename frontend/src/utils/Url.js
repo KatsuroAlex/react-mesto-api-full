c09991273a900a4e39a6762b9d20765ee6115356
KatsuroAlex <katsuroay@mail.ru>
2023-03-24 19:56:45 +0400

    https was changed for http

--- frontend/src/utils/Url.js
+++ frontend/src/utils/Url.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://katsuroproject15.nomoredomains.work';
+export const BASE_URL = 'http://katsuroproject15.nomoredomains.work';
 // export const BASE_URL = 'http://localhost:3000';
