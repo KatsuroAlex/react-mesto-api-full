13bb2ffed4779eb87c36664f7c413a20bdc1311f
KatsuroAlex <katsuroay@mail.ru>
2023-03-29 23:06:48 +0400

    build in backend

--- backend/app.js
+++ backend/app.js
@@ -20,8 +20,8 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 // app.use('/api', require('../router'));
-// app.use(express.static(path.join(__dirname, 'build')));
-app.use(express.static(path.join(__dirname, '../frontend/build')));  
+app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, '../frontend/build')));  
 app.use(bodyParser.json());
 
 // // подключаем мидлвары, роуты и тд
