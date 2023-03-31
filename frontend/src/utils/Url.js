2a4486b3cd448bc2622cec4e28f859110dd684a1
KatsuroAlex <katsuroay@mail.ru>
2023-03-31 15:07:26 +0400

    frontend url changed

--- frontend/src/utils/Url.js
+++ frontend/src/utils/Url.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://katsuroproject15.nomoredomains.work';
-// export const BASE_URL = 'http://Katsuroprojectbackend15.nomoredomains.work';
+// export const BASE_URL = 'http://katsuroproject15.nomoredomains.work';
+export const BASE_URL = 'http://Katsuroprojectbackend15.nomoredomains.work';
 // export const BASE_URL = 'http://localhost:3000';
