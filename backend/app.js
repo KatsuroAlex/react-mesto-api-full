7c5a444e4e8fec6a409d95739ca7c66b2b2ad8cc
KatsuroAlex <katsuroay@mail.ru>
2023-04-01 19:50:37 +0400

    mongo

--- backend/app.js
+++ backend/app.js
@@ -60,7 +60,7 @@ app.use(handleErrors);
 // подключаемся к серверу mongo
 mongoose.set('strictQuery', true);
 // mongoose.connect('mongodb://localhost:27017/mestodb', {
-mongoose.connect('mongodb://127.0.0.1/mestodb', {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 
 }, () => {
   app.listen(PORT, () => {
